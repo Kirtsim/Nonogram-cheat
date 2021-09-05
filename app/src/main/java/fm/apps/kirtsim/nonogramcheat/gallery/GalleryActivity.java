6de1178daf820f12d4e1b5ae258825f946717f12
Filip Mistrik <filipmistrik@gmail.com>
2021-09-05 13:17:25 +0100

    Project upgrade.
    - upgraded gradle to version 7.0.2
    - dependencies/libraries upgraded to their latest version
    - migrated Android Support library to AndroidX
    - bumped SDK version to the latest API lvl 31 (Android 12.0 - S)

--- app/src/main/java/fm/apps/kirtsim/nonogramcheat/gallery/GalleryActivity.java
+++ app/src/main/java/fm/apps/kirtsim/nonogramcheat/gallery/GalleryActivity.java
@@ -1,7 +1,7 @@
 package fm.apps.kirtsim.nonogramcheat.gallery;
 
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
--- app/src/main/java/fm/apps/kirtsim/nonogramcheat/solving/SolverActivity.java
+++ app/src/main/java/fm/apps/kirtsim/nonogramcheat/solving/SolverActivity.java
@@ -1,11 +1,11 @@
 package fm.apps.kirtsim.nonogramcheat.solving;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
